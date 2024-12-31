94a8b188fd3440872ca67d20fb42044d7c21fd6a
Eric Gu <ejgu@umass.edu>
2024-12-31 17:38:11 -0500

    put output files into folder

--- extract_audio.py
+++ extract_audio.py
@@ -1,6 +1,7 @@
 from sheetMusicMIDI import findSeconds
 from to_wav import convert_to_wav
 from pydub import AudioSegment
+import os
 
 # Function to calculate appropriate start time and end time
 # IMPORTANT: Under assumption multiple notes can not be played at the same time due to
@@ -8,18 +9,18 @@ from pydub import AudioSegment
 def boundary_times(times, duration):
     # times is list of times in seconds; duration in milliseconds
     result = []
-    for i in range(len(times) - 1):
+    for i in range(len(times)):
         # time format is (note name, start time in seconds, start time in quarter notes)
-        if len(times - 1):
-            start_time = (times[i][1] * 1000) - 100
+        if i == len(times) - 1:
+            start_time = (times[i][1] * 1000) - 5
             end_time = duration
         else:
-            if times[i][1] == 0:
-                start_time = 0
+            if (times[i][1] * 1000) == 0.0:
+                start_time = 0.0
             else:
-                start_time = times[i][1] - 100
+                start_time = (times[i][1] * 1000) - 5
 
-            end_time = times[i + 1][1] - 100
+            end_time = (times[i + 1][1] * 1000) - 5
 
         result.append([start_time, end_time])
 
@@ -36,16 +37,20 @@ def extract_audio_segment(input_file, start_time_ms, end_time_ms, output_file):
 
 def extract_audio(sheet_music_filepath, audio_filepath):
     # Get filepath to sheet music and output notes and timestamps--> TODO: get input from user
-    # sheet_music_filepath = "/Users/guja/Coding/NoteRecognition/Someone You Loved.xml"
     times = findSeconds(sheet_music_filepath)
     
     audio_file = audio_filepath
     # Find the duration in milliseconds
     duration = len(AudioSegment.from_file(audio_file))
 
+    # Directory where the audio segments will be saved
+    output_directory = "extracted_audio_segments"
+
+    # Create the directory if it doesn't exist
+    os.makedirs(output_directory, exist_ok=True)
+
     time_intervals = boundary_times(times, duration)
-    for intervals in time_intervals:
-        extract_audio_segment(audio_file, intervals[0], intervals[1])
-
-
+    for i in range(len(time_intervals)):
+        output_file = os.path.join(output_directory, f"testTest{i}.wav")
+        extract_audio_segment(audio_file, time_intervals[i][0], time_intervals[i][1], output_file)
 

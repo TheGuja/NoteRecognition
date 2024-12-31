c4ff0e4e205f64bb9814adbd8f5b99d1eac27656
Eric Gu <ejgu@umass.edu>
2024-12-31 15:12:07 -0500

    takes in general audio file and converts to .m4a

---
+++ to_wav.py
@@ -0,0 +1,9 @@
+from pydub import AudioSegment
+
+def convert_m4a_to_wav(input_file, output_file, target_sample_rate=16000):
+    audio = AudioSegment.from_file(input_file)
+
+    if target_sample_rate:
+        audio = audio.set_frame_rate(target_sample_rate)
+
+    audio.export(output_file, format="wav")

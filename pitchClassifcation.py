353c5ea5a36241631fb2772b27a0e27ad024cfcf
Eric Gu <ejgu@umass.edu>
2025-01-08 17:40:13 -0500

    changed function so that it doesn't repeatedly calls load on a model

--- pitchClassifcation.py
+++ pitchClassifcation.py
@@ -1,15 +1,12 @@
 from CNNModel import CNNModel
 import torch
-from torch.utils.data import DataLoader
-from nsynthDataset import NSynthCustom
 import torchaudio
 import torch.nn.functional as F
-import nsynthDataset
 
-def pitchClassification(filepath):
+def pitchClassification(model_filepath):
     # Load the model with 88 classes
     loaded_model = CNNModel(88)
-    loaded_model.load_state_dict(torch.load(f="/Users/guja/Coding/NoteRecognition/models/nsynthCNNModel_5_epochs.pth"))
+    loaded_model.load_state_dict(torch.load(f=model_filepath))
 
     # Specify device
     device = "mps"
@@ -18,35 +15,40 @@ def pitchClassification(filepath):
 
     # Put model in evaluation mode
     loaded_model.eval()
-    with torch.inference_mode():
-        # Transformation function for .wav file --> sample rate is 16000
-        transformation = torchaudio.transforms.MelSpectrogram(16000)
 
-        # Load the audio
-        waveform, sr = torchaudio.load(filepath)
+    def classify(audio_filepath):
+        with torch.inference_mode():
+            # Transformation function for .wav file --> sample rate is 16000
+            mel_transform = torchaudio.transforms.MelSpectrogram(sample_rate=16000)
 
-        # Resample audio if needed
-        waveform = torchaudio.transforms.Resample(orig_freq=sr, new_freq=16000)(waveform)
+            # Load the audio
+            waveform, sr = torchaudio.load(audio_filepath)
 
-        # Apply melspectrogram transformation to waveform
-        mel_spectrogram = transformation(waveform)
+            # Resample audio if needed
+            waveform = torchaudio.transforms.Resample(orig_freq=sr, new_freq=16000)(waveform)
 
-        # Pad with zeros at the end of the time dimension to fit tensor input size
-        mel_spectrogram = F.pad(mel_spectrogram, (0, 321 - mel_spectrogram.shape[2]))
-        mel_spectrogram = mel_spectrogram.unsqueeze(0)
+            # Apply melspectrogram transformation to waveform
+            mel_spectrogram = mel_transform(waveform)
 
-        # Move to mps
-        mel_spectrogram = mel_spectrogram.to(device)
+            # Pad with zeros at the end of the time dimension to fit tensor input size
+            mel_spectrogram = F.pad(mel_spectrogram, (0, 321 - mel_spectrogram.shape[2]))
+            mel_spectrogram = mel_spectrogram.unsqueeze(0)
 
-        # Output results
-        outputs = loaded_model(mel_spectrogram)
-        _, predicted = torch.max(outputs, 1)
+            # Move to mps
+            mel_spectrogram = mel_spectrogram.to(device)
+
+            # Output results
+            outputs = loaded_model(mel_spectrogram)
+            _, predicted = torch.max(outputs, 1)
+            
+            return predicted
         
-        return predicted
+    return classify
 
 if __name__ == "__main__":
-    transformation = torchaudio.transforms.MelSpectrogram(16000)
-    train_data_custom = nsynthDataset.NSynthCustom(targ_dir="/Users/guja/Coding/NoteRecognition/data/test", transform=transformation)
-    print(train_data_custom.class_to_idx)
+    # transformation = torchaudio.transforms.MelSpectrogram(16000)
+    # train_data_custom = nsynthDataset.NSynthCustom(targ_dir="/Users/guja/Coding/NoteRecognition/data/test", transform=transformation)
+    # print(train_data_custom.class_to_idx)
 
-    print(pitchClassification("/Users/guja/Coding/NoteRecognition/testTest0.wav"))
+    model = pitchClassification("/Users/guja/Coding/NoteRecognition/models/nsynthCNNModel_4ConvBlocks_5_epochs.pth")
+    print(model("/Users/guja/Coding/NoteRecognition/C.wav"))
